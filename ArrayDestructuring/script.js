202c08b453c4ed74edbcc2a1f97b9e56cede7dd9
reddirani8496 <reddiranibattuluri@gmail.com>
2024-06-05 22:51:44 +0530

    Array destructuring program uploaded..

---
+++ ArrayDestructuring/script.js
@@ -0,0 +1,23 @@
+const colors = ["red", "green", "yellow", "blue"];
+
+// printing two colors
+// let [color1, color2] = colors; // let color1 = "red", color2 = "green
+
+// console.log(color1); // red
+// console.log(color2); // green
+
+// printing all values
+
+// let [color1, color2, color3, color4] = colors;
+
+// console.log(color1); // red
+// console.log(color2); // green
+// console.log(color3); // yellow
+// console.log(color4); // blue
+
+// using rest operator
+
+let [color1, ...rest] = colors;
+
+console.log(color1); // red
+console.log(rest); // [ 'green', 'yellow', 'blue' ]
