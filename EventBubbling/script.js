69a799a97674c1773eaecf46c7b9a80af0ac4eaa
reddirani8496 <reddiranibattuluri@gmail.com>
2024-06-12 22:33:17 +0530

    event bubbling program uploaded..

---
+++ EventBubbling/script.js
@@ -0,0 +1,25 @@
+let button = document.getElementsByTagName("button")[0];
+let span = document.getElementsByTagName("span")[0];
+let div = document.getElementsByTagName("div")[0];
+let body = document.getElementsByTagName("body")[0];
+let html = document.getElementsByTagName("html")[0];
+
+html.addEventListener("click", () => {
+  console.log("html was clicked");
+});
+
+body.addEventListener("click", () => {
+  console.log("body was clicked");
+});
+
+div.addEventListener("click", () => {
+  console.log("div was clicked");
+});
+
+span.addEventListener("click", () => {
+  console.log("span was clicked");
+});
+
+button.addEventListener("click", () => {
+  console.log("button was clicked");
+});
