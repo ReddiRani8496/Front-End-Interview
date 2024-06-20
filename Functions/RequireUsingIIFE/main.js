cb0273baa1ffb32d8cc9dd6285283e1ac5d7e4d7
reddirani8496 <reddiranibattuluri@gmail.com>
2024-06-21 03:14:30 +0530

    require program using IIFE uploaded..

---
+++ Functions/RequireUsingIIFE/main.js
@@ -0,0 +1,3 @@
+let rectangle = require("./rectangle");
+
+console.log(rectangle.area(10, 20));
---
+++ Functions/RequireUsingIIFE/rectangle.js
@@ -0,0 +1,11 @@
+let area = function (length, breadth) {
+  return length * breadth;
+};
+
+(function () {
+  console.log("Hello from Rectange file");
+})();
+
+module.exports = {
+  area,
+};
